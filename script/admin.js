500047c3ed3dc1f6b1d98d4ed61e6e7121aa0e88
Alonso0213 <alonsocupido51@gmail.com>
2023-06-15 10:57:09 +0200

    fixing bugs

--- script/admin.js
+++ script/admin.js
@@ -2,9 +2,11 @@ const addBtn = document.querySelector('#save');
 const productInput = document.querySelector('#product-Input');
 let products = JSON.parse(localStorage.getItem('content'));
 const displayElement = document.querySelector('#content');
+// For add new product
+
 function mfisher(){
   displayElement.innerHTML = "";
-  products.forEach(content => {
+  products.forEach((content, i) => {
   //  console.log(displayElement); 
   displayElement.innerHTML+=`
 <tr>
@@ -52,7 +54,7 @@ function mfisher(){
     </div>
   </div>
 </div></button>
-<button type="button" class="btn btn-primary" id="remove-item" onclick="">Delete</button>
+<button type="button" class="btn btn-primary" id="remove-item" onclick='deleteItems(${JSON.stringify(i)})'>Delete</button>
 </td>
 
 </tr> 
@@ -65,16 +67,15 @@ mfisher()
 
 function EditProduct(item) {
   this.id= item.id;
-  this.Name = document.querySelector("#name${item.id}").value;
-  this.description = document.querySelector("#description${item.id}").value;
-  this.Price = document.querySelector("#price${item.id}").value;
-  this.img = document.querySelector("#img${item.id}").value;
+  this.Name = document.querySelector(`#name${item.id}`).value;
+  this.description = document.querySelector(`#description${item.id}`).value;
+  this.Price = document.querySelector(`#price${item.id}`).value;
+  this.img = document.querySelector(`#img${item.id}`).value;
 
   let index = products.findIndex( p=> {
     p.id == item.id;
 
   })
-
   // Update
   content[index] = Object.assign({}, this)
   localStorage.setItem('content',JSON.stringify(content));
@@ -94,46 +95,26 @@ mfisher()
 
 
 function addData() {
-  let Name = document.querySelector(Name).value;
-  let description = document.querySelector(description).value;
-  let Price = document.querySelector(Price).value;
-  let img = document.querySelector(img).value;
-  
-  let content;
-  if (localStorage.getItem("content")==null) {
-    content =[];
-  }
-  else{
-    content = JSON.parse(localStorage.getItem("content"))
-  }
-  
-  content.push({
-    Name : Name,
-    description: description,
-    Price: Price,
-    img: img,
-
+  let Name = document.querySelector('#productName');
+let description = document.querySelector('#productDescription');
+let Price = document.querySelector('#productPrice');
+let img = document.querySelector('#productImage');
+    products.push({
+    Name : Name.value,
+    description: description.value,
+    Price: Price.value,
+    img: img.value,
   })
-  localStorage.setItem("content", JSON.stringify(content))
+  localStorage.setItem("content", JSON.stringify(products))
+  location.reload()
   addData()
-  document.getElementById("#name${item.id}").value;
-  document.getElementById("#description${item.id}").value;
-  document.getElementById("#price${item.id}").value;
-  document.getElementById("#img${item.id}").value;
 
 }
 
-function deleteItems(index) {
-  let content;
-  if (localStorage.getItem("content")==null) {
-    content =[];
-  }
-  else{
-    content = JSON.parse(localStorage.getItem("content"))
-  }
 
-  content.splice(index, 1);
-  localStorage.setItem("content", JSON.stringify(content))
+function deleteItems(index) {
+  products.splice(index, 1);
+  localStorage.setItem("content", JSON.stringify(products))
   addData()
 }
 
--- script/code.js
+++ script/code.js
@@ -8,7 +8,7 @@ let content = JSON.parse(localStorage.getItem("content"))
         Name:"Play Staion 5",
         Price:"R12999,00",
         description:"CPU: 3.5GHz, 8-core AMD Zen 2 <br> GPU: 10.3 teraflop RDNA 2 GPU <br>RAM: 16GB GDDR6.<br>Storage: Custom 825GB SSD.<br>Expansion: NVMe M.2 SSD slot<br>Disc drive: 4K Blu-ray player.<br>Size: 15.4 x 10.2 x 4.1 inches",
-        img:"https://media.gamestop.com/i/gamestop/20001549",
+        img:"https://i.postimg.cc/0jV4YYfK/11206901-11206900.jpg",
        
     },
     { 
@@ -16,7 +16,7 @@ let content = JSON.parse(localStorage.getItem("content"))
       Name:"Spider-Man Miles Morales",
       Price:"R1 259.00",
       description:"Spider-Man Miles Morales For Ps5",
-      img:"https://www.popsci.com/uploads/2021/07/21/sony-marvels-spider-man-best-ps5-games.jpg?auto=webp",
+      img:"https://i.postimg.cc/rwBPNFSj/20001549.jpg",
      
   },
   { 
@@ -24,16 +24,17 @@ let content = JSON.parse(localStorage.getItem("content"))
     Name:"God of War Ragnarok ",
     Price:"R999.00",
     description:"God of War Ragnarok (Lauch Edition) for Ps5",
-    img:"https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/p/s/ps5_gowr_ste_pksht_ft_au_rgb_220614.png",
+    img:"https://i.postimg.cc/pL4gcBtD/71qw-Expw-Fk-L-AC-UF1000-1000-QL80.jpg"
+    ,
    
-},
+   },
     
     { 
         id: 4,
         Name:"Play station 4 Pro",
         Price:"R5 899,00",
         description:"Main processor. Single-chip custom processor. CPU : x86-64 AMD “Jaguar”, 8 cores <br>Memory. GDDR5 8GB Storage size* 500GB, 1TB. <br>BD/ DVD drive. (read only) BD × 6 CAV",
-        img:"https://cdn.images.express.co.uk/img/dynamic/143/590x/PS4-Pro-officially-announced-Sony-confirms-PS4-NEO-release-date-and-price-708306.jpg",
+        img:"https://i.postimg.cc/NM7SMq24/PS4-Pro-officially-announced-Sony-confirms-PS4-NEO-release-date-and-price-708306.jpg",
        
     },
     { 
@@ -41,7 +42,7 @@ let content = JSON.parse(localStorage.getItem("content"))
         Name:"Call of Duty MW",
         Price:"R740.00",
         description:"Call of Duty for Ps4",
-        img:"https://media.gamestop.com/i/gamestop/11206901-11206900?$plp-tile3x$",
+        img:"https://i.postimg.cc/BZpRf30Z/ps5-gowr-ste-pksht-ft-au-rgb-220614.png",
        
     },
     { 
@@ -49,33 +50,59 @@ let content = JSON.parse(localStorage.getItem("content"))
         Name:"Mortal Kombat 11",
         Price:"R389.00",
         description:"Mortal Kombat 11 for Ps4",
-        img:"https://m.media-amazon.com/images/I/71qwExpwFkL._AC_UF1000,1000_QL80_.jpg",
+        img:"https://i.postimg.cc/Y0rVLn1w/sony-marvels-spider-man-best-ps5-games.webp",
        
-    }
+    },
+    { 
+      id: 7,
+      Name:"Mobius Headset",
+      Price:"R4 999.00",
+      description:"Mobius is powered by Audeze Planar Magnetic drivers, internationally acclaimed for their superior sound quality. Experience a level of depth and realism in your game audio that no other video game headphones can even hope to match",
+      img:"https://i.postimg.cc/JzHXZjcy/audeze-mobius-headset.jpg",
+     
+  },
+  { 
+    id: 8,
+    Name:"Maxwell Gaming Headset",
+    Price:"R7 999.00",
+    description:"Mortal Kombat 11 for Ps4",
+    img:"https://i.postimg.cc/YqY5Ds3M/xbox-audeze-gaming-headset.jpg",
+   
+},
+{ 
+  id: 9,
+  Name:"Xbox series S 512GB",
+  Price:"R6 999.00",
+  description:"",
+  img:"https://i.postimg.cc/pLN2pj0X/series-s-gilded-hunter.jpg",
+ 
+},
+{ 
+  id: 10,
+  Name:"Starfield(XBS)",
+  Price:"R1 399.00",
+  description:"Mortal Kombat 11 for Ps4",
+  img:"https://i.postimg.cc/3RWRgT2K/xbs-starfield-tbc-pack.jpg",
+ 
+}
 ])
 )
 
 let displayElement = document.querySelector('#content');
 content.forEach(content => {
-   console.log(displayElement); displayElement.innerHTML+=`
+   console.log(displayElement); 
+   displayElement.innerHTML +=`
    
-   <div class="col-3">
-     <div class="card"><div class="img">
-       <img src=" ${content.img}" class="card-img-top" alt="..."  >
-      </div>
-       <div class="card-body">
-         <h5 class="card-title">${content.Name}</h5>
-         <p class="card-text">${content.Price}</p>
-         <p class="card-text">${content.description}</p>
-         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-         Add to cart
-       </button>
-       </div>
-     </div>
-   </div>
+   <div class="card" style="width: 18rem;" >
+  <img src="${content.img}" class="card-img-top" alt="icon">
+  <div class="card-body">
+    <p class="card-text">${content.description}</p>
+    
+  </div>
+  <p class="card-text">${content.Price}</p>
+   <button type="button" class="btn btn-primary" id="save" onclick="AddData()">Buy</button>
+</div>
 
-   
-  
 `});
 localStorage.setItem("content",JSON.stringify(content));
 
@@ -84,9 +111,10 @@ function mfisher(){
   display.innerHTML = "";
   products.forEach(content => {
   //  console.log(displayElement); 
-  display.innerHTML+=`
+  display.innerHTML +=`
   <div class="col-3">
-  <div class="card"><div class="img">
+  <div class="card">
+  <div class="img">
     <img src=" ${content.img}" class="card-img-top" alt="..."  >
    </div>
     <div class="card-body">
